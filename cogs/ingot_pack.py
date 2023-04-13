dfa86826f488daec93cfd6eae70af4c4a1365d3a
averagebrine <84639405+averagebrine@users.noreply.github.com>
2023-04-13 15:08:48 -0400

    Refactored search command

--- cogs/ingot_pack.py
+++ cogs/ingot_pack.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from PIL import Image
 
 backing_image_path = "assets/display/backing.png"
-temp_image_path = "assets/temp/temp.png"
+temporary_image_path = "assets/temporary/temporary.png"
 random_ingots_path = "assets/display/random_ingots/"
 
 class IngotPackCommands(commands.Cog):
@@ -15,64 +15,71 @@ class IngotPackCommands(commands.Cog):
         self,
         ctx,
         mode: discord.Option(str, choices=["item", "emoji"]),
-        query: discord.Option(str),
-        raw: discord.Option(bool, required=False, default=False)
+        name: discord.Option(str),
+        raw:  discord.Option(bool, required=False, default=False)
         ):
 
-        ingot_name = str(query).lower().replace(" ", "_")
+        ingot_name = str(name).lower().replace("_", " ")
+        ingot_file_name = str(name).lower().replace(" ", "_")
 
-        if ingot_name == "turtle" or ingot_name == "vedal" or ingot_name == "hello":
-            embed = discord.Embed(title=ingot_name.title().replace("_", " "), color=discord.Colour.green())
-            embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/1093280397162455101.gif?quality=lossless")
-            final_image = discord.File("assets/display/vedal.gif")
+        if ingot_name == "hello" or ingot_name == "turtle" or ingot_name == "vedal":
+            embed = discord.Embed(color=discord.Colour.green())
+            embed_file = discord.File("assets/display/vedal.gif")
             embed.set_image(url="attachment://vedal.gif")
-            embed.set_footer(text=ingot_name.title().replace("_", " "), icon_url="https://cdn.discordapp.com/emojis/1093280397162455101.gif?quality=lossless")
-
-            await ctx.respond(embed=embed, file=final_image)
+            await ctx.respond(embed=embed, file=embed_file)
             return
 
         if mode == "item":
-            ingot_texture_path = "https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/minecraft/textures/item/" + ingot_name + ".png"
+            ingot_url = "https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/minecraft/textures/item/" + ingot_file_name + ".png"
         else:
-            ingot_texture_path = "https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/ingot_cult/emoji/" + ingot_name + ".png"
+            ingot_url = "https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/ingot_cult/emoji/" + ingot_file_name + ".png"
 
-        urllib.request.urlretrieve(ingot_texture_path, temp_image_path)
-
-        embed = discord.Embed(title=ingot_name.title().replace("_", " "), color=discord.Colour.blurple(), url=ingot_texture_path)
+        try:
+            urllib.request.urlretrieve(ingot_url, temporary_image_path)
+        except Exception as error:
+            if "404" in str(error):
+                await ctx.respond("Sorry, that ingot doesn't seem to exist. Are you sure you spelled it correctly?", ephemeral=True)
+            else:
+                await ctx.respond("Uh oh, there's been some kind of error. Please try again.", ephemeral=True)
+                print(error)
+            return
 
         # ingo descripto
-        ingot_description = requests.get('https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/ingot_cult/ing-bot/descriptions/' + ingot_name + '.txt').content.decode('utf-8')
-        if ingot_description != '404: Not Found':
-            embed.description = ingot_description
+        ingot_description = requests.get('https://raw.githubusercontent.com/WaspVentMan/Ingot-Pack/main/assets/ingot_cult/ing-bot/descriptions/' + ingot_file_name + '.txt').content.decode('utf-8')
 
         # dumb rendering time!!!!
         if mode == "emoji":
             if not raw:
-                ingot_image = Image.open(temp_image_path)
+                ingot_image = Image.open(temporary_image_path)
                 ingot_image = ingot_image.resize((256, 256), 0)
-                ingot_image.save(temp_image_path)
+                ingot_image.save(temporary_image_path)
         elif not raw:
-
-            backing_image = Image.open(backing_image_path)
-            ingot_image = Image.open(temp_image_path).convert("RGBA")
+            base_image = Image.open(backing_image_path).convert("RGBA")
+            ingot_image = Image.open(temporary_image_path).convert("RGBA")
 
             random_ingots = []
             for i in range(8):
                 random_ingot_path = random_ingots_path + random.choice(os.listdir(random_ingots_path))
                 random_ingots.append(Image.open(random_ingot_path).convert("RGBA"))
 
-            backing_image.paste(ingot_image, (6, 6), ingot_image)
+            base_image.paste(ingot_image, (6, 6), ingot_image)
+    
             random_ingot_positions = [(-12, -12), (6, -12), (24, -12), (-12, 6), (24, 6), (-12, 24), (6, 24), (24, 24)]
-            for i in range(len(random_ingots)):
-                backing_image.paste(random_ingots[i], random_ingot_positions[i], random_ingots[i])
+            for i in range(8):
+                base_image.paste(random_ingots[i], random_ingot_positions[i], random_ingots[i])
 
-            backing_image = backing_image.resize((504, 504), 0)
-            backing_image.save(temp_image_path)
+            base_image = base_image.resize((504, 504), 0)
+            base_image.save(temporary_image_path)
 
-        final_image = discord.File("assets/temp/temp.png", filename="ingot.png")
+        embed = discord.Embed(title=ingot_name.title(), color=discord.Colour.blurple(), url=ingot_url)
+        if ingot_description != '404: Not Found':
+            embed.description = ingot_description
+        embed_file = discord.File(temporary_image_path, filename="ingot.png")
         embed.set_image(url="attachment://ingot.png")
 
-        await ctx.respond(embed=embed, file=final_image)
+        await ctx.respond(embed=embed, file=embed_file)
+        image = Image.new(mode="RGBA", size=(16, 16))
+        image.save(temporary_image_path)
 
 def setup(bot):
     bot.add_cog(IngotPackCommands(bot))
